da6c6d2e249f15f53dc25bb3c93eeace0be697df
muhammad Rusdi <muhamamdrusdi@icloud.com>
2018-07-08 16:32:17 +0800

    update footer

--- src/dpm-ptsp/assets/scripts/app.js
+++ src/dpm-ptsp/assets/scripts/app.js
@@ -3,4 +3,11 @@ $(function() {
     $('nav .nav-bottom').toggleClass('active');
     $('body').toggleClass('fixed');
   })
+
+  $(window).on('resize', (e) => {
+    if (e.target.innerWidth >= 640) {
+      $('nav .nav-bottom').removeClass('active');
+      $('body').removeClass('fixed');
+    }
+  })
 });
