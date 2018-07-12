2022fdc9d3da62e1e27b81dccfbdfe0b1a93779f
Gifa <gifaeriyanto@ymail.com>
2018-07-12 14:01:38 -0700

    PTSP Makassar rebuild

--- src/dpm-ptsp/assets/scripts/app.js
+++ src/dpm-ptsp/assets/scripts/app.js
@@ -1,13 +1,11 @@
-$(function() {
-  $('.toggle-nav').on('click', () => {
-    $('nav .nav-bottom').toggleClass('active');
-    $('body').toggleClass('fixed');
-  })
+$('.toggle-nav').on('click', function() {
+  $('nav .nav-bottom').toggleClass('active');
+  $('body').toggleClass('fixed');
+});
 
-  $(window).on('resize', (e) => {
-    if (e.target.innerWidth >= 640) {
-      $('nav .nav-bottom').removeClass('active');
-      $('body').removeClass('fixed');
-    }
-  })
+$(window).on('resize', function(e) {
+  if (e.target.innerWidth >= 640) {
+    $('nav .nav-bottom').removeClass('active');
+    $('body').removeClass('fixed');
+  }
 });
