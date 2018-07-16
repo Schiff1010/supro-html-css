d577880038aa4f0e3f29826f998bfce10bc7eabe
Gifa <gifaeriyanto@ymail.com>
2018-07-16 15:50:03 -0700

    Mall Pengayoman - Home (not responsive)

--- src/mall-pengayoman/assets/scripts/app.js
+++ src/mall-pengayoman/assets/scripts/app.js
@@ -12,3 +12,32 @@ $('.category .dropdown-category li').each(function () {
     })
   })
 })
+
+// Owl Carousel
+$('.slide .owl-carousel').owlCarousel({
+  items: 1,
+  loop: true,
+  nav: true,
+  autoplay: true,
+  navText: ['<i class="material-icons">chevron_left</i>', '<i class="material-icons">chevron_right</i>']
+})
+$('.product .owl-carousel').owlCarousel({
+  items: 5,
+  nav: true,
+  navText: ['<i class="material-icons">chevron_left</i>', '<i class="material-icons">chevron_right</i>'],
+  responsive: {
+    0: {
+      items: 2
+    },
+    767: {
+      items: 3
+    },
+    992: {
+      items: 4
+    },
+    1200: {
+      items: 5
+    }
+    
+  }
+})
